a9ea5dff30ee08bb4dcdcfdae9e21b1f5d16e60b
Luis Fernando <lsh.fernandors@gmail.com>
2020-05-19 16:41:02 -0300

    Updates

---
+++ precache-manifest.5918ec6f142cb25b5f03d77120b23bd4.js
@@ -0,0 +1,62 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7fff6d84649ee13149c11ec9c301c81d",
+    "url": "/portifolio-app/index.html"
+  },
+  {
+    "revision": "def8eb876b76a2ef6fe3",
+    "url": "/portifolio-app/static/css/main.41bdb7aa.chunk.css"
+  },
+  {
+    "revision": "76f252d89d2c47ce6543",
+    "url": "/portifolio-app/static/js/2.5bc83773.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/portifolio-app/static/js/2.5bc83773.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "def8eb876b76a2ef6fe3",
+    "url": "/portifolio-app/static/js/main.c4fa1aa5.chunk.js"
+  },
+  {
+    "revision": "91248c7902453feb3f3f",
+    "url": "/portifolio-app/static/js/runtime-main.e42fb738.js"
+  },
+  {
+    "revision": "42cf9f0820d16f3ac2c26a7710ce70f2",
+    "url": "/portifolio-app/static/media/Poppins-Black.42cf9f08.ttf"
+  },
+  {
+    "revision": "c23534acbeddbaadfd0ab2d2bbfdfc84",
+    "url": "/portifolio-app/static/media/Poppins-Bold.c23534ac.ttf"
+  },
+  {
+    "revision": "6b78c7ec468eb0e13c6c5c4c39203986",
+    "url": "/portifolio-app/static/media/Poppins-ExtraBold.6b78c7ec.ttf"
+  },
+  {
+    "revision": "2a47a29ceb33c966c8d79f8d5a5ea448",
+    "url": "/portifolio-app/static/media/Poppins-Light.2a47a29c.ttf"
+  },
+  {
+    "revision": "ba95810b56f476990ca71d15139d5111",
+    "url": "/portifolio-app/static/media/Poppins-Medium.ba95810b.ttf"
+  },
+  {
+    "revision": "41e8dead03fb979ecc23b8dfb0fef627",
+    "url": "/portifolio-app/static/media/Poppins-Regular.41e8dead.ttf"
+  },
+  {
+    "revision": "342ba3d8ac29ac8c38d7cef8efbf2dc9",
+    "url": "/portifolio-app/static/media/Poppins-SemiBold.342ba3d8.ttf"
+  },
+  {
+    "revision": "c0fafa8397437c95848724aed686d63b",
+    "url": "/portifolio-app/static/media/Poppins-Thin.c0fafa83.ttf"
+  },
+  {
+    "revision": "6269dd0310816c1f171c563b9b228a8e",
+    "url": "/portifolio-app/static/media/bcc.6269dd03.png"
+  }
+]);
